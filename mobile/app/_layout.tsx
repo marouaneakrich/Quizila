0fc3cb8b3d6d7d1f9621d7ec2f9c67e4f233468e
marouane.akrich <82279645+marouaneakrich@users.noreply.github.com>
2026-01-15 10:34:34 +0100

    QUIZ-69 Initialize Expo Project

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+import { useAuthStore } from "../src/store/auth.store";
+
+export default function RootLayout() {
+  const isAuthenticated = useAuthStore(
+    state => state.isAuthenticated
+  );
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {!isAuthenticated ? (
+        <Stack.Screen name="(auth)" />
+      ) : (
+        <Stack.Screen name="(tabs)" />
+      )}
+    </Stack>
+  );
+}
